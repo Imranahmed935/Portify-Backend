1e6501371575e82a442fce858005d4bf361dd109
Imranahmed935 <imrantahir9918@gmail.com>
2025-10-15 17:23:01 +0600

    portify-backend setup done

---
+++ src/server.ts
@@ -0,0 +1,33 @@
+import http, { Server } from "http";
+import dotenv from "dotenv";
+import app from "./app";
+import { prisma } from "./config/db";
+
+
+dotenv.config();
+let server: Server | null = null;
+
+async function connectToDB() {
+  try {
+    await prisma.$connect();
+    console.log("*** DB connection successfull!!")
+  } catch (error) {
+    console.log("*** DB connection failed!")
+    process.exit(1);
+  }
+}
+
+async function startServer() {
+  try {
+    await connectToDB()
+    server = http.createServer(app);
+    server.listen(process.env.PORT, () => {
+      console.log(`The Server is Running on the Port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error("❌ Error during server startup:", error);
+    process.exit(1);
+  }
+}
+
+startServer();
