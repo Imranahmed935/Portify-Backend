d1c4b53ba0aa06263e9ecb919fc74f022521de72
Imranahmed935 <imrantahir9918@gmail.com>
2025-10-16 18:50:19 +0600

    jwt authorization done

--- src/App/modules/blog/blog.route.ts
+++ src/App/modules/blog/blog.route.ts
@@ -1,14 +1,16 @@
 import { Router } from "express";
 import { blogController } from "./blog.controller";
+import { checkAuth } from "../../middleware/checkAuth";
+import { Role } from "@prisma/client";
 
 
 const router = Router();
 
-router.post("/create", blogController.createBlog);
-router.get("/", blogController.getAllBlog);
-router.get("/:id", blogController.getBlogById);
-router.put("/:id", blogController.updateBlog);
-router.delete("/:id", blogController.deleteBlog);
+router.post("/create", checkAuth(Role.SUPER_ADMIN), blogController.createBlog);
+router.get("/", checkAuth(Role.SUPER_ADMIN), blogController.getAllBlog);
+router.get("/:id", checkAuth(Role.SUPER_ADMIN), blogController.getBlogById);
+router.put("/:id", checkAuth(Role.SUPER_ADMIN), blogController.updateBlog);
+router.delete("/:id", checkAuth(Role.SUPER_ADMIN), blogController.deleteBlog);
 
 
 export const blogRouter = router;
